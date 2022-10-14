f08f088426432a6ca739072954a0973c4127568c
Filipe Fraqueiro <59435851+FilipeFraqueiro@users.noreply.github.com>
2022-10-14 12:15:36 +0100

    Update guessinggame.sh

--- guessinggame.sh
+++ guessinggame.sh
@@ -1,1 +1,25 @@
+n=$(ls -p | grep -v / | wc -l)
+# echo $n
 
+function test() {
+	echo "Guess:"
+	read in
+	# echo $in
+
+	if [ $n -eq $in ]
+	then
+		echo "Well Done!"
+		return 0
+	else
+		return 1
+	fi
+}
+
+
+state=1
+
+while [ $state -eq 1 ]
+do
+	test
+	state=$?
+done
